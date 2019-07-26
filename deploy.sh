0bef6cd6f2e40bb533510c08665308d0e84e6e0f
noendfoli <49503264+noendfoli@users.noreply.github.com>
2019-07-26 16:41:58 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
-kill $(lsof -t -i:8080) || true
+pid=$(lsof -t -i:8080)
+if [ -z "$pid" ]
+then
+      echo "\$pid is NULL"
+else
+      echo "\$pid is NOT NULL"
+      kill -9 $pid
+fi
 cp -f build/libs/todolist-0.0.1-SNAPSHOT.jar /var/lib/jenkins/todolist-0.0.1-SNAPSHOT.jar
 nohup java -jar /var/lib/jenkins/todolist-0.0.1-SNAPSHOT.jar  > log.file  2>&1 &
